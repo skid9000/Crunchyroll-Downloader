9c75cb1b42090d19627ba49ca9acbf5cd62e41c6
Skid <skid@tuto-craft.com>
2017-10-12 23:49:58 +0200

    First commit

---
+++ Crunchyroll-Downloader/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Cookie_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "Crunchyroll Cookie Text File (*.txt)|*.txt";
+
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                cookie_TextBox.Text = filename;
+            }
+
+
+        }
+
+
+        private void button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".mp4";
+            dlg.Filter = "Such mp4, such wow | *.mp4";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                save_TextBox.Text = filename;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Program Machin = new Program();
+
+
+            Machin.langue = comboBox.Text;
+            Machin.format = comboBox_Copy.Text;
+            Machin.cookie = cookie_TextBox.Text;
+            Machin.url = textBox.Text;
+            Machin.savePath = save_TextBox.Text;
+
+            if (String.IsNullOrEmpty(Machin.url))
+            {
+                MessageBox.Show("Please, put a URL.", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            if (String.IsNullOrEmpty(Machin.savePath))
+            {
+                MessageBox.Show("Please, put a save path.", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            if (String.IsNullOrEmpty(Machin.cookie))
+            {
+                MessageBox.Show("Please, put a cookie file.", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            if (String.IsNullOrEmpty(Machin.langue))
+            {
+                MessageBox.Show("Please, choose a language.", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+            if (String.IsNullOrEmpty(Machin.format))
+            {
+                MessageBox.Show("Please, choose a sub format.", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                return;
+            }
+
+            Machin.FirstStep();
+
+
+
+        }
+
+        private void aboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("All informations on github.", "About", MessageBoxButton.OK);
+        }
+    }
+}
---
+++ Crunchyroll-Downloader/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
+
+namespace WpfApplication1
+{
+    public class Program
+    {
+        public void FirstStep()
+        {
+
+            string ActualFolder = @"%USERPROFILE%\AppData\Roaming\Crunchy-DL";
+            WebClient Client = new WebClient();
+            var x = new ICSharpCode.SharpZipLib.Zip.FastZip();
+
+            if (Directory.Exists(@"%USERPROFILE%\AppData\Roaming\Crunchy-DL"))
+            {
+                Downloading();
+            }
+            else
+            {
+                MessageBox.Show("Youtube-DL & Ffmpeg not detected, downloading ...", "Important Note", MessageBoxButton.OK, MessageBoxImage.Information);
+                Directory.CreateDirectory(@"%USERPROFILE%\AppData\Roaming\Crunchy-DL");
+                Client.DownloadFile("https://github.com/rg3/youtube-dl/releases/download/2017.10.12/youtube-dl.exe", @"%USERPROFILE%\AppData\Roaming\Crunchy-DL\youtube-dl.exe");
+                Client.DownloadFile("http://download.tucr.tk/ffmpeg.zip", @"%USERPROFILE%\AppData\Roaming\Crunchy-DL\ffmpeg.zip");
+
+                x.ExtractZip(ActualFolder + @"\ffmpeg.zip", ActualFolder, "");
+
+
+
+            }
+        }
+
+        public string url { get; set; }
+        public string cookie { get; set; }
+        public string langue { get; set; }
+        public string format { get; set; }
+        public string savePath { get; set; }
+
+        private void Downloading()
+        {
+            MessageBox.Show("Everything is good, a pop up will tell you when the downloadis finished.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = @"%USERPROFILE%\AppData\Roaming\Crunchy-DL\youtube-dl.exe";
+            process.StartInfo.Arguments = "--write-sub --sub-lang " + langue + " --sub-format " + format + " --no-part -o " + '"' + savePath + '"' + " --cookies " + '"' + cookie + '"' + " " + url;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            process.Start();
+            process.WaitForExit();// Waits here for the process to exit.
+            MessageBox.Show("Download finished !", "Success !", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+        }
+
+
+    }
+}
