e53f65b6cec870fce2e531002225a6f4e1b7f5b8
Skid <skid@tuto-craft.com>
2018-09-05 15:56:21 +0200

    Repair indent + fix some errors.

--- Crunchyroll-Downloader/MainWindow.xaml.cs
+++ Crunchyroll-Downloader/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.Win32;
 using System;
 using System.ComponentModel;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
 
 namespace CrunchyrollDownloader
 {
@@ -16,7 +18,7 @@ namespace CrunchyrollDownloader
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-        public MainWindow()
+		public MainWindow()
 		{
 			var actualFolder = @"C:\ProgramData\Crunchy-DL";
 			using (var client = new WebClient())
@@ -94,63 +96,62 @@ namespace CrunchyrollDownloader
 			process.WaitForExit(); // Waits here for the process to exit.
 		}
 
-        public string dl_label { get; private set; }
+		public string dl_label { get; private set; }
 
-        private void InstallAll()
+		private void InstallAll()
 		{
-            var viewerThread = new Thread(() =>
-            {
-                var download_window = new DownloadWindow();
-                download_window.MyString = dl_label;
-                download_window.Show();
-                download_window.Activate();
-                download_window.Closed += (s, e) =>
-                Dispatcher.CurrentDispatcher.BeginInvokeShutdown(DispatcherPriority.Normal);
-                Dispatcher.Run();
-            });
-            viewerThread.SetApartmentState(ApartmentState.STA); // needs to be STA or throws exception
-            
-            var actualFolder = @"C:\ProgramData\Crunchy-DL";
-            using (var client = new WebClient())
-            {
-                var zip = new FastZip();
-                MessageBox.Show("Dependencies not detected, downloading ...", "Important Note", MessageBoxButton.OK, MessageBoxImage.Information);
-                Directory.CreateDirectory(@"C:\ProgramData\Crunchy-DL");
-                dl_label="[1/3] Downloading dependencies : Youtube-DL ...";
-                viewerThread.Start();
-                client.DownloadFile("https://yt-dl.org/downloads/latest/youtube-dl.exe", @"C:\ProgramData\Crunchy-DL\youtube-dl.exe");
-                viewerThread.Abort();
-                dl_label = "[2/3] Downloading dependencies : FFmpeg ...";
-                viewerThread.Start();
-                client.DownloadFile("https://raw.githubusercontent.com/skid9000/Crunchyroll-Downloader/develop/FFmpeg/ffmpeg.zip", @"C:\ProgramData\Crunchy-DL\ffmpeg.zip");
+			var viewerThread = new Thread(() =>
+			{
+				var download_window = new DownloadWindow();
+				download_window.MyString = dl_label;
+				download_window.Show();
+				download_window.Activate();
+				download_window.Closed += (s, e) =>
+				Dispatcher.CurrentDispatcher.BeginInvokeShutdown(DispatcherPriority.Normal);
+				Dispatcher.Run();
+			});
+			viewerThread.SetApartmentState(ApartmentState.STA); // needs to be STA or throws exception
+			
+			var actualFolder = @"C:\ProgramData\Crunchy-DL";
+			using (var client = new WebClient())
+			{
+				var zip = new FastZip();
+				MessageBox.Show("Dependencies not detected, downloading ...", "Important Note", MessageBoxButton.OK, MessageBoxImage.Information);
+				Directory.CreateDirectory(@"C:\ProgramData\Crunchy-DL");
+				dl_label="[1/3] Downloading dependencies : Youtube-DL ...";
+				viewerThread.Start();
+				client.DownloadFile("https://yt-dl.org/downloads/latest/youtube-dl.exe", @"C:\ProgramData\Crunchy-DL\youtube-dl.exe");
+				viewerThread.Abort();
+				dl_label = "[2/3] Downloading dependencies : FFmpeg ...";
+				viewerThread.Start();
+				client.DownloadFile("https://raw.githubusercontent.com/skid9000/Crunchyroll-Downloader/develop/FFmpeg/ffmpeg.zip", @"C:\ProgramData\Crunchy-DL\ffmpeg.zip");
 				client.DownloadFile("https://raw.githubusercontent.com/skid9000/Crunchyroll-Downloader/develop/FFmpeg/ffplay.zip", @"C:\ProgramData\Crunchy-DL\ffplay.zip");
 				client.DownloadFile("https://raw.githubusercontent.com/skid9000/Crunchyroll-Downloader/develop/FFmpeg/ffprobe.zip", @"C:\ProgramData\Crunchy-DL\ffprobe.zip");
-                viewerThread.Abort();
-                dl_label = "[3/3] Downloading dependencies : Crunchyroll-Auth ...";
-                viewerThread.Start();
-                client.DownloadFile("https://github.com/skid9000/CrunchyrollAuth/releases/download/1.0/login.zip", @"C:\ProgramData\Crunchy-DL\login.zip");
-                viewerThread.Abort();
+				viewerThread.Abort();
+				dl_label = "[3/3] Downloading dependencies : Crunchyroll-Auth ...";
+				viewerThread.Start();
+				client.DownloadFile("https://github.com/skid9000/CrunchyrollAuth/releases/download/1.0/login.zip", @"C:\ProgramData\Crunchy-DL\login.zip");
+				viewerThread.Abort();
 
-                dl_label = "Extracting ...";
-                viewerThread.Start();
-                zip.ExtractZip(actualFolder + @"\ffmpeg.zip", actualFolder, "");
+				dl_label = "Extracting ...";
+				viewerThread.Start();
+				zip.ExtractZip(actualFolder + @"\ffmpeg.zip", actualFolder, "");
 				zip.ExtractZip(actualFolder + @"\ffplay.zip", actualFolder, "");
 				zip.ExtractZip(actualFolder + @"\ffprobe.zip", actualFolder, "");
-                zip.ExtractZip(actualFolder + @"\login.zip", actualFolder, "");
-                viewerThread.Abort();
-                MessageBox.Show("youtube-dl and FFmpeg are now installed.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-                InitializeComponent();
-                CheckCookie();
-            }
-        }
+				zip.ExtractZip(actualFolder + @"\login.zip", actualFolder, "");
+				viewerThread.Abort();
+				MessageBox.Show("youtube-dl and FFmpeg are now installed.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+				InitializeComponent();
+				CheckCookie();
+			}
+		}
 
 		private void button_Save_Click(object sender, RoutedEventArgs e)
 		{
-            // Create OpenFileDialog
-                var dialog = new FolderBrowserDialog();
-                var result = dialog.ShowDialog();
-                if (result ?? false)
-                    save_TextBox.Text = dialog.SelectedPath;
+			// Create OpenFileDialog
+				var dialog = new FolderBrowserDialog();
+				var result = dialog.ShowDialog();
+				save_TextBox.Text = dialog.SelectedPath;
 		}
 
 		private void button_Click(object sender, RoutedEventArgs e)
@@ -176,23 +177,23 @@ namespace CrunchyrollDownloader
 			else if (comboBox.Text == "Русский")
 				machin.Langue = "ruRU";
 
-            if (QualitycomboBox.Text == "Best (recommended)")
-                machin.Quality = "best";
-            else if (QualitycomboBox.Text == "1080p")
-                machin.Quality = "1080";
-            else if (QualitycomboBox.Text == "720p")
-                machin.Quality = "720";
-            else if (QualitycomboBox.Text == "480p")
-                machin.Quality = "480";
-            else if (QualitycomboBox.Text == "360p")
-                machin.Quality = "360";
+			if (QualitycomboBox.Text == "Best (recommended)")
+				machin.Quality = "best";
+			else if (QualitycomboBox.Text == "1080p")
+				machin.Quality = "1080";
+			else if (QualitycomboBox.Text == "720p")
+				machin.Quality = "720";
+			else if (QualitycomboBox.Text == "480p")
+				machin.Quality = "480";
+			else if (QualitycomboBox.Text == "360p")
+				machin.Quality = "360";
 
-            if (MkvcheckBox.IsChecked.Value == true)
-                machin.MkvStatus = "1";
-            else
-                machin.MkvStatus = "0";
+			if (MkvcheckBox.IsChecked.Value == true)
+				machin.MkvStatus = "1";
+			else
+				machin.MkvStatus = "0";
 
-            machin.Format = comboBox_Copy.Text;
+			machin.Format = comboBox_Copy.Text;
 			machin.Url = urlBox.Text;
 			machin.SavePath = save_TextBox.Text;
 			machin.STState = "0";
@@ -256,9 +257,9 @@ namespace CrunchyrollDownloader
 		{
 			comboBox.IsEnabled = checkBox.IsChecked.Value;
 			comboBox_Copy.IsEnabled = checkBox.IsChecked.Value;
-            MkvcheckBox.IsEnabled = checkBox.IsChecked.Value;
+			MkvcheckBox.IsEnabled = checkBox.IsChecked.Value;
 
-        }
+		}
 		private void checkBox_Checked(object sender, RoutedEventArgs e) => CheckBoxChanged();
 		private void checkBox_Unchecked(object sender, RoutedEventArgs e) => CheckBoxChanged();
 
@@ -275,5 +276,5 @@ namespace CrunchyrollDownloader
 			button_login.IsEnabled = true;
 			button_logout.IsEnabled = false;
 		}
-    }
+	}
 }
